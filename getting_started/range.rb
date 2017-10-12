692690691885d8f96b142f3bf6c2f343e3d9da62
tp00012x <anthony_torres01@yahoo.com>
2017-10-12 18:12:28 -0500

    finished getting started

---
+++ getting_started/range.rb
@@ -0,0 +1,20 @@
+nums = 10..20
+
+p nums.class
+puts nums
+
+nums2 = 1...5
+p nums2
+
+#Methods
+
+p nums.first
+p nums.last
+
+p nums.first(5)
+
+#Alphabetical Ranges
+
+puts (1..10).first(3)
+
+p (50..100)
---
+++ getting_started/statement_modifiers.rb
@@ -0,0 +1,7 @@
+number = 5000
+
+if number > 2500
+  puts 'Huge number'
+end
+
+puts 'Huge number!' if number > 2500
